46d004d6a682d77e6ba396a3960f133bb6be5744
Parthiv93 <parthivmanoj93@gmail.com>
2023-07-15 13:32:09 +0530

    added while loop

--- hangman.py
+++ hangman.py
@@ -6,9 +6,17 @@ wordlen=len(randchose)
 for i in range(wordlen):
     blank.append("_")
 print("Welcome to hangman movie guesser. You have to guess the movie before the man gets hanged")
-guessword=input("Guess any letter : ").lower()
-for i in range(wordlen):
-    letter=randchose[i]
-    if letter==guessword:
-        blank[i]=letter
+print("Guess the blank spaces of the letters of the movie below")
 print(blank)
+endofgame=False
+while(not endofgame):
+    guessword=input("Insert any letter : ").lower()
+    for i in range(wordlen):
+        letter=randchose[i]
+        if letter==guessword:
+            blank[i]=letter
+    print(blank)
+    if "_" not in blank:
+        endofgame=True
+        print("You won")
+        
