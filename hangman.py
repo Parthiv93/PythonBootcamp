268e518e0f73a66baf5724925e8e1eeda82f8774
Parthiv93 <parthivmanoj93@gmail.com>
2023-07-15 13:48:33 +0530

    done B-)

--- hangman.py
+++ hangman.py
@@ -1,4 +1,61 @@
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+lives=6
 words=["transformers","avengers","batman","oppenheimer","barbie"]
 blank=[]
 randchose=random.choice(words)
@@ -6,6 +63,7 @@ wordlen=len(randchose)
 for i in range(wordlen):
     blank.append("_")
 print("Welcome to hangman movie guesser. You have to guess the movie before the man gets hanged")
+print(stages[0])
 print("Guess the blank spaces of the letters of the movie below")
 print(blank)
 endofgame=False
@@ -15,6 +73,13 @@ while(not endofgame):
         letter=randchose[i]
         if letter==guessword:
             blank[i]=letter
+    if guessword not in randchose:
+            lives-=1
+            if lives==0:
+               endofgame=True
+               print("You lost, the man got hanged :(")
+            else:
+               print(stages[len(stages)-lives])
     print(blank)
     if "_" not in blank:
         endofgame=True
