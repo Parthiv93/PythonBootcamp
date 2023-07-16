1ed5208419dd12aa9fd031e9f3da0ac92ca28c09
Parthiv93 <parthivmanoj93@gmail.com>
2023-07-16 20:30:37 +0530

    added ascii

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,5 +1,5 @@
-import art
-print(art.logo)
+import caesarcipherart
+print(caesarcipherart.logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 j="yes"
 while j!="no":
