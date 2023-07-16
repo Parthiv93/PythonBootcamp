ca6047a9b87cb795d008a038b612590c8e4bc4d1
Parthiv93 <parthivmanoj93@gmail.com>
2023-07-16 16:57:39 +0530

    merged encrypt and decrypt into a single function

--- caesarcipher.py
+++ caesarcipher.py
@@ -2,23 +2,20 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-def encrypt(plain_text,shift_no):
+def cipher(plain_text,shift_no,shift_direction):
     cipher_text=""
-    for i in plain_text:
-        position=alphabet.index(i)
-        new_position=position+shift_no
-        new_letter=alphabet[new_position]
-        cipher_text+=new_letter
-    print("The encrypted message is : "+cipher_text) 
-def decrypt(cipher_text,shift_back):
-    decrypt_text=""
-    for i in cipher_text:
-        position=alphabet.index(i)
-        new_position=position-shift_back
-        new_letter=alphabet[new_position]
-        decrypt_text+=new_letter
-    print("The decrypted message is : "+decrypt_text)
-if direction=="encode":
-    encrypt(text,shift)
-else:
-    decrypt(text,shift)
+    if shift_direction=="encode":
+        for i in plain_text:
+            position=alphabet.index(i)
+            new_position=position+shift_no
+            new_letter=alphabet[new_position]
+            cipher_text+=new_letter
+        print("The encrypted message is : "+cipher_text) 
+    else:
+        for i in plain_text:
+            position=alphabet.index(i)
+            new_position=position-shift_no
+            new_letter=alphabet[new_position]
+            cipher_text+=new_letter
+        print("The decrypted message is : "+cipher_text)
+cipher(text,shift,direction)
