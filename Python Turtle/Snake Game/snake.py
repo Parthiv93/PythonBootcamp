41aad776d78a7822065ea283c25ca3609ea0e26c
Parthiv93 <parthivmanoj93@gmail.com>
2023-09-01 13:53:30 +0530

    Control snake direction using keybind

--- Python Turtle/Snake Game/snake.py
+++ Python Turtle/Snake Game/snake.py
@@ -6,6 +6,7 @@ class Snake:
     def __init__(self):
         self.segments=[]
         self.create_snake()
+        self.head=self.segments[0]
         
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -20,5 +21,21 @@ class Snake:
             new_x=self.segments[seg_num-1].xcor()
             new_y=self.segments[seg_num-1].ycor()
             self.segments[seg_num].goto(new_x,new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+        
+    def up(self):
+        if self.head.heading()!=270:
+            self.head.setheading(90)
+    
+    def down(self):
+        if self.head.heading()!=90:
+            self.head.setheading(270)
+            
+    def left(self):
+        if self.head.heading()!=0:
+            self.head.setheading(180)
+            
+    def right(self):
+        if self.head.heading()!=180:
+            self.head.setheading(0)
     
--- Python Turtle/Snake Game/snake_game.py
+++ Python Turtle/Snake Game/snake_game.py
@@ -8,6 +8,14 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake=Snake()  
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+
  
 game_is_on=True
 while game_is_on:
